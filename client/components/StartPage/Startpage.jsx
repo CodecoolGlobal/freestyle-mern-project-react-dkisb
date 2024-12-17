c93ddd9849d279859a47021b94a95633dccc58cf
dkisb <177304332+dkisb@users.noreply.github.com>
2024-12-17 14:40:28 +0100

    implement start page

---
+++ client/components/StartPage/Startpage.jsx
@@ -0,0 +1,18 @@
+import './StartPage.css';
+
+function StartPage() {
+  return (
+    <>
+      <div className="start-page">
+        <button className="start-button">Start Game</button>
+      </div>
+      <div className="bottom-buttons">
+        <button>Rules</button>
+        <button>My Stats</button>
+        <button>Help</button>
+      </div>
+    </>
+  );
+}
+
+export default StartPage;
