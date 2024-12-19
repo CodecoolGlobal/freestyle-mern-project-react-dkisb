76ce64408d978fefcab66cf9019bb8cc1b014a7e
bencsicszoli <171580038+bencsicszoli@users.noreply.github.com>
2024-12-19 14:24:33 +0100

    Implement button functions

--- client/components/GamePage/DisplayButtons.jsx
+++ client/components/GamePage/DisplayButtons.jsx
@@ -5,18 +5,28 @@ function handleH2PClick() {
 }
 
 
-function DisplayButtons({onHandleMore, yourHandValue, onHandleStop}) {
+function DisplayButtons({onHandleMore, onHandleAiMore, yourHandValue, dealerHandValue, onHandleStop, stopClicked}) {
   return (
     <div>
       <div className="game-focused-buttons">
         {yourHandValue < 20 && <button onClick={onHandleMore}>More</button>}
         
         <button>Raise bet</button>
+        
         {yourHandValue >= 15 && <button onClick={onHandleStop}>Stop</button>}
       </div>
       <div className="help-button">
         <button onClick={handleH2PClick}>How to play</button>
       </div>
+      {(stopClicked && dealerHandValue < 15) &&
+      <div className="ai-more-button">
+        <button onClick={onHandleAiMore}>More</button>
+      </div>}
+      {(stopClicked && dealerHandValue >= 15 && dealerHandValue <= 21) &&
+      <div className="ai-enough-button">
+        <button>Enough</button>
+      </div>}
+
     </div>
   );
 }
--- client/components/GamePage/GamePage.jsx
+++ client/components/GamePage/GamePage.jsx
@@ -2,7 +2,7 @@ import './GamePage.css';
 import DisplayBalances from './DisplayBalances';
 import Cards from './Cards';
 import DisplayButtons from './DisplayButtons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Gamepage({randomCards}) {
   const [yourHand, setYourHand] = useState([]);
@@ -12,7 +12,8 @@ function Gamepage({randomCards}) {
   const [yourHandData, setYourHandData] = useState([]);
   const [dealerHandData, setDealerHandData] = useState([]);
   const [yourHandValue, setYourHandValue] = useState(0);
-  const [dealerHandValue, setDealerHandValue] = useState(0)
+  const [dealerHandValue, setDealerHandValue] = useState(0);
+  const [stopClicked, setStopClicked] = useState(false)
   async function handleMore() {
     setYourHand([...yourHand, randomCardIds[0]]);
     randomCardIds.splice(0, 1);
@@ -23,14 +24,41 @@ function Gamepage({randomCards}) {
     setYourHandData([...yourHandData, cardData]);
   }
 
-  function handleStop() {
-     console.log('Stop button');
+  async function handleAiMore() {
+    setDealerHand([...dealerHand, randomCardIds[0]]);
+    randomCardIds.splice(0, 1);
+    setRandomCardIds([...randomCardIds]);
+    const response = await fetch(`/api/cards/${randomCardIds[0]}`);
+    const cardData = await response.json();
+    setUpperCardData(cardData);
+    setDealerHandData([...dealerHandData, cardData]);
   }
+
+  async function handleStop() {
+     console.log('Stop button');
+     setStopClicked(true);
+     setDealerHand([...dealerHand, randomCardIds[0]]);
+     randomCardIds.splice(0, 1);
+     setRandomCardIds([...randomCardIds]);
+     const response = await fetch(`/api/cards/${randomCardIds[0]}`);
+     const cardData = await response.json();
+     setUpperCardData(cardData);
+     setDealerHandData([...dealerHandData, cardData]);
+  }
+  // useEffect(() => {
+  //   if (upperCardData && stopClicked && dealerHandValue < 15) {
+  //     setDealerHandValue(dealerHandValue + upperCardData.value);
+  //     console.log(dealerHandValue)
+  //   } //else if (upperCard && stopClicked) {
+  //     //onSetDealerValue(dealerHandValue + upperCard.value)
+  //   //}
+  // }, [upperCardData]);
+
   return (
     <div>
       <DisplayBalances />
-      <Cards yourHandValue={yourHandValue} onSetValue={setYourHandValue} card={randomCardIds[0]} upperCard={upperCardData} numberOfCards={randomCardIds.length} yourHand={yourHand} yourHandData={yourHandData}/>
-      <DisplayButtons yourHandValue={yourHandValue} onHandleStop={handleStop} onHandleMore={handleMore}/>
+      <Cards yourHandValue={yourHandValue} dealerHandValue={dealerHandValue} stopClicked={stopClicked} onSetYourValue={setYourHandValue} onSetDealerValue={setDealerHandValue} card={randomCardIds[0]} upperCard={upperCardData} numberOfCards={randomCardIds.length} yourHand={yourHand} yourHandData={yourHandData} dealerHandData={dealerHandData}/>
+      <DisplayButtons dealerHandValue={dealerHandValue} stopClicked={stopClicked} yourHandValue={yourHandValue} onHandleStop={handleStop} onHandleMore={handleMore} onHandleAiMore={handleAiMore}/>
     </div>
   );
 }
