04ec9980dbb7c1a94f2b77ff438015f6d8058436
dkisb <177304332+dkisb@users.noreply.github.com>
2024-12-19 16:09:25 +0100

    fix merging issues and bugs

--- client/components/GamePage/Gamepage.jsx
+++ client/components/GamePage/Gamepage.jsx
@@ -4,7 +4,7 @@ import Cards from './Cards';
 import DisplayButtons from './DisplayButtons';
 import { useState, useEffect } from 'react';
 
-function Gamepage({randomCards}) {
+function Gamepage({ randomCards }) {
   const [yourHand, setYourHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
   const [randomCardIds, setRandomCardIds] = useState(randomCards);
@@ -13,7 +13,10 @@ function Gamepage({randomCards}) {
   const [dealerHandData, setDealerHandData] = useState([]);
   const [yourHandValue, setYourHandValue] = useState(0);
   const [dealerHandValue, setDealerHandValue] = useState(0);
-  const [stopClicked, setStopClicked] = useState(false)
+  const [stopClicked, setStopClicked] = useState(false);
+  const [dealerBalance, setDealerBalance] = useState(100);
+  const [playerBalance, setPlayerBalance] = useState(100);
+  const [totalBet, setTotalBet] = useState(0);
   async function handleMore() {
     setYourHand([...yourHand, randomCardIds[0]]);
     randomCardIds.splice(0, 1);
@@ -35,21 +38,46 @@ function Gamepage({randomCards}) {
   }
 
   async function handleStop() {
-     console.log('Stop button');
-     setStopClicked(true);
-     setDealerHand([...dealerHand, randomCardIds[0]]);
-     randomCardIds.splice(0, 1);
-     setRandomCardIds([...randomCardIds]);
-     const response = await fetch(`/api/cards/${randomCardIds[0]}`);
-     const cardData = await response.json();
-     setUpperCardData(cardData);
-     setDealerHandData([...dealerHandData, cardData]);
+    console.log('Stop button');
+    setStopClicked(true);
+    setDealerHand([...dealerHand, randomCardIds[0]]);
+    randomCardIds.splice(0, 1);
+    setRandomCardIds([...randomCardIds]);
+    const response = await fetch(`/api/cards/${randomCardIds[0]}`);
+    const cardData = await response.json();
+    setUpperCardData(cardData);
+    setDealerHandData([...dealerHandData, cardData]);
   }
   return (
     <div>
-      <DisplayBalances />
-      <Cards yourHandValue={yourHandValue} dealerHandValue={dealerHandValue} stopClicked={stopClicked} onSetYourValue={setYourHandValue} onSetDealerValue={setDealerHandValue} card={randomCardIds[0]} upperCard={upperCardData} numberOfCards={randomCardIds.length} yourHand={yourHand} yourHandData={yourHandData} dealerHandData={dealerHandData}/>
-      <DisplayButtons dealerHandValue={dealerHandValue} stopClicked={stopClicked} yourHandValue={yourHandValue} onHandleStop={handleStop} onHandleMore={handleMore} onHandleAiMore={handleAiMore}/>
+      <DisplayBalances dealerMax={dealerBalance} playerMax={playerBalance} currentTotal={totalBet} />
+      <Cards
+        yourHandValue={yourHandValue}
+        dealerHandValue={dealerHandValue}
+        stopClicked={stopClicked}
+        onSetYourValue={setYourHandValue}
+        onSetDealerValue={setDealerHandValue}
+        card={randomCardIds[0]}
+        upperCard={upperCardData}
+        numberOfCards={randomCardIds.length}
+        yourHand={yourHand}
+        yourHandData={yourHandData}
+        dealerHandData={dealerHandData}
+      />
+      <DisplayButtons
+        dealerHandValue={dealerHandValue}
+        stopClicked={stopClicked}
+        yourHandValue={yourHandValue}
+        onHandleStop={handleStop}
+        onHandleMore={handleMore}
+        onHandleAiMore={handleAiMore}
+        dealerMax={dealerBalance}
+        playerMax={playerBalance}
+        currentTotal={totalBet}
+        onBet={setTotalBet}
+        onSetDealer={setDealerBalance}
+        onSetPlayer={setPlayerBalance}
+      />
     </div>
   );
 }
