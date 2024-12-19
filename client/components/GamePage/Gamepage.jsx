a8900d805f90837454f64cd3f1f895d752a19474
dkisb <177304332+dkisb@users.noreply.github.com>
2024-12-19 09:18:55 +0100

    merge resolve

--- client/components/GamePage/Gamepage.jsx
+++ client/components/GamePage/Gamepage.jsx
@@ -1,13 +1,13 @@
 import './Gamepage.css';
 import DisplayBalances from './DisplayBalances';
-import DisplayCards from './DisplayCards';
+import Cards from './Cards';
 import DisplayButtons from './DisplayButtons';
 
 function Gamepage() {
   return (
     <div>
       <DisplayBalances />
-      <DisplayCards />
+      <Cards />
       <DisplayButtons />
     </div>
   );
